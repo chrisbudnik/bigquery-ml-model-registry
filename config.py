a9dd3611fe9899cca9386cc19170c6f6acad8628
chrisb <chrism.budnik@gmail.com>
2023-09-06 23:29:42 +0200

    create: config

---
+++ config.py
@@ -0,0 +1,18 @@
+import os
+from google.cloud import bigquery
+
+class Config:
+    """Base class to handle configuration and authentication."""
+    
+    _client = None
+
+    @property
+    def client(self):
+        if Config._client is None:
+            path_to_keys = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+
+            if path_to_keys is None:
+                raise EnvironmentError("The GOOGLE_APPLICATION_CREDENTIALS environment variable is not set.")
+            Config._client = bigquery.Client.from_service_account_json(path_to_keys)
+            
+        return Config._client
