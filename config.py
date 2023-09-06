cda8295f3c64c1b76f7273e6467cee9189a4d9bc
chrisb <chrism.budnik@gmail.com>
2023-09-07 00:18:11 +0200

    update: change var names

--- config.py
+++ config.py
@@ -3,16 +3,15 @@ from google.cloud import bigquery
 
 class Config:
     """Base class to handle configuration and authentication."""
-    
     _client = None
 
     @property
     def client(self):
         if Config._client is None:
-            path_to_keys = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS")
+            path_to_keys = os.environ.get("SERVICE_ACCOUNT_CREDENTIALS")
 
             if path_to_keys is None:
-                raise EnvironmentError("The GOOGLE_APPLICATION_CREDENTIALS environment variable is not set.")
+                raise EnvironmentError("The GSERVICE_ACCOUNT_CREDENTIALS environment variable is not set.")
             Config._client = bigquery.Client.from_service_account_json(path_to_keys)
-            
+
         return Config._client
