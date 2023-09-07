7e6c98db5ddae6a40243ff24d372eeb265b8e3d4
chrisb <chrism.budnik@gmail.com>
2023-09-07 22:04:45 +0200

    update: model data and model registry use cases

--- example.py
+++ example.py
@@ -1,14 +1,32 @@
 from model_registry import ModelRegistry
+from model_data import ModelData
 
 # Example usage
 if __name__ == "__main__":
-    path_to_keys = "path/to/service-account-key.json"
     project_id = "your_project_id"
     dataset_id = "dataset_id"
     table_id = "model_registry"
 
-    registry = ModelRegistry(path_to_keys, project_id, dataset_id, table_id)
+    # Create model registry table
+    registry = ModelRegistry(project_id, dataset_id, table_id)
+    registry.init_table()
     
     # Add a model to the registry
     model_id = "model_name_1"
+    model = ModelData(project_id, dataset_id, model_id)
+
+    # Inspect metadata
+    print(model.metadata)
+
+    # Fetch hyperparameters
+    hyperparams = model.fetch_hyperparams()
+    print(hyperparams)
+
+    # Fetch feature importance - only tree models
+    importances = model.fetch_feature_importance()
+    print(importances)
+
+    # Adding model to registry (all metadata)
     registry.add_model(model_id)
+
+    
