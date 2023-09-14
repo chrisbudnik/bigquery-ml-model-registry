dcc5e85a77668bd3f64522ede75296fec54687ae
chrisb <chrism.budnik@gmail.com>
2023-09-15 00:05:44 +0200

    update: helper method - replace table

--- model_registry.py
+++ model_registry.py
@@ -1,5 +1,6 @@
 from typing import List, Dict, Union, Optional, Literal
 from google.cloud import bigquery
+from google.api_core.exceptions import NotFound
 from config import Config
 from model_data import ModelData
 from schemas import RegistrySchema
@@ -13,14 +14,24 @@ class ModelRegistry(Config):
         self.dataset_id = dataset_id
         self.table_id = table_id
         self.full_table_id = f"{project_id}.{dataset_id}.{table_id}"
+
+    def _replace_table(self, schema: RegistrySchema) -> None:
+        """Replacee table based on provided schema."""
+
+        self.client.delete_table(self.full_table_id, not_found_ok=True)
+        self.client.create_table(bigquery.Table(self.full_table_id, schema=schema.value))
+        print(f"Table: {self.full_table_id} replaced.")
     
     def init_table(self, schema: RegistrySchema, replace: bool = False) -> None:
         """Initialize or validate the registry table."""
-        # add replace param
+
         try:
             self.client.get_table(self.full_table_id)
             print(f"Table: {self.full_table_id} already exists!")
-        except:
+
+            if replace: self._replace_table(schema)
+
+        except NotFound:
             self.client.create_table(bigquery.Table(self.full_table_id, schema=schema.value))
             print(f"Table: {self.full_table_id} successfully created.")
 
