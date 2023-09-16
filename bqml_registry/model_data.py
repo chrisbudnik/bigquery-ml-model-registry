1a2250ad7a5fcf62de142b21218907a278fb695b
chrisb <chrism.budnik@gmail.com>
2023-09-16 16:30:16 +0200

    feature: generate model sql

--- bqml_registry/model_data.py
+++ bqml_registry/model_data.py
@@ -157,5 +157,40 @@ class ModelData(Config):
             FROM ML.FEATURE_IMPORTANCE(MODEL `{self.project_id}.{self.dataset_id}.{self.model_id}`)
         """
         return self.query(feature_importance_sql)
+    
+    def execute_information_schema_sql(self, region: str = "us") -> pd.DataFrame:
+        """Executes and fetches results from INFORMATION_SCHEMA.JOBS_BY_PROJECT view."""
+
+        information_schema_sql = f"""
+            SELECT * 
+            FROM `chris-sandbox-2023.region-{region}.INFORMATION_SCHEMA.JOBS_BY_PROJECT`
+            WHERE project_id = "{self.project_id}"
+        """
+        return self.query(information_schema_sql)
+    
+    def execute_search_model_sql(self, region: str = "us") -> pd.DataFrame:
+        """Executes query on INFORMATION_SCHEMA and searches for model creation statement."""
+
+        search_model_sql = f"""
+            SELECT *
+            FROM `chris-sandbox-2023.region-{region}.INFORMATION_SCHEMA.JOBS_BY_PROJECT`
+            WHERE project_id = "{self.project_id}"
+                AND statement_type = "CREATE_MODEL"
+                AND state = "DONE"
+                AND destination_table.table_id = "{self.model_id}"
+            
+            ORDER BY creation_time DESC
+            LIMIT 1
+        """
+        return self.query(search_model_sql)
+
+    def generate_model_sql(self, region: str = "us") -> str:
+        """Retrive model create statement sql from information schema."""
+
+        model_info = self.execute_search_model_sql(region)
+        return "".join(model_info["query"].to_list())
+        
+
+
 
 
