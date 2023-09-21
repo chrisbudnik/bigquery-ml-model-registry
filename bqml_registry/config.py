6b10fcf8a5c20d1dd6ca8a425ec501b34ec353d3
chrisb <chrism.budnik@gmail.com>
2023-09-21 18:58:19 +0200

    feature: check permissions method

--- bqml_registry/config.py
+++ bqml_registry/config.py
@@ -1,23 +1,48 @@
 import os
 import pandas as pd
 from google.cloud import bigquery
+from googleapiclient import discovery
+from google.oauth2 import service_account
 
 class Config:
     """Base class to handle configuration and authentication."""
     _client = None
+    _credentials = None
+
+    @property
+    def credentials(self):
+        if Config._credentials is None:
+            path_to_keys = os.environ.get("SERVICE_ACCOUNT_CREDENTIALS")
+            if path_to_keys is None:
+                raise EnvironmentError("The SERVICE_ACCOUNT_CREDENTIALS environment variable is not set.")
+            
+            Config._credentials = service_account.Credentials.from_service_account_file(
+                path_to_keys,
+                scopes=["https://www.googleapis.com/auth/cloud-platform"]
+            )
+        return Config._credentials
 
     @property
     def client(self):
         if Config._client is None:
-            path_to_keys = os.environ.get("SERVICE_ACCOUNT_CREDENTIALS")
-
-            if path_to_keys is None:
-                raise EnvironmentError("The SERVICE_ACCOUNT_CREDENTIALS environment variable is not set.")
-            Config._client = bigquery.Client.from_service_account_json(path_to_keys)
-
+            Config._client = bigquery.Client(credentials=self.credentials)
         return Config._client
+
+    def check_permissions(self, permissions: list) -> bool:
+        """Check if the service account has the required permissions."""
+        service = discovery.build('cloudresourcemanager', 'v1', credentials=self.credentials)
+        body = {
+            'permissions': permissions
+        }
+        resource = f"{self.client.project}"
+        request = service.projects().testIamPermissions(resource=resource, body=body)
+        response = request.execute()
+
+        # Check if all required permissions are granted
+        return set(permissions).issubset(set(response.get('permissions', [])))
     
     def query(self, sql: str) -> pd.DataFrame:
         """Query BigQuery tables with sql and save results into DataFrame."""
         return self.client.query(sql).to_dataframe()
-    
+
+
