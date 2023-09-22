fc1aec391b16df7c429c01ad94990d5db67c339e
chrisb <chrism.budnik@gmail.com>
2023-09-22 19:33:42 +0200

    add: SQLNotFoundError

--- bqml_registry/exceptions.py
+++ bqml_registry/exceptions.py
@@ -2,11 +2,17 @@ class RegistryError(Exception):
     """Base class for registry-related exceptions"""
     pass
 
-class PermissionsError(RegistryError):
+class BigQueryPermissionError(RegistryError):
     """Exception raised for permission-related errors."""
 
     def __init__(self, message="Setup error occurred"):
         self.message = message
         super().__init__(self.message)
 
+class SQLNotFoundError(RegistryError):
+    """Exception raised for model sql not found errors."""
+
+    def __init__(self, message="Model not found"):
+        self.message = message
+        super().__init__(self.message)
 
