77d4984525f8973aa7681770b9ad4788222730cf
chrisb <chrism.budnik@gmail.com>
2023-09-22 19:37:30 +0200

    update: include new erros

--- bqml_registry/__init__.py
+++ bqml_registry/__init__.py
@@ -3,3 +3,6 @@ from .model_data import ModelData
 from .model_names import ModelNames
 from .model_registry import ModelRegistry
 from .schemas import RegistrySchema
+from .connector import BigQueryConnector
+from .exceptions import BigQueryPermissionError, SQLNotFoundError
+from .permissions import RequiredPermissions
