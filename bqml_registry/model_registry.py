dd5cefa459121da5a7e0d80b6889d6512cda7358
chrisb <chrism.budnik@gmail.com>
2023-09-23 23:28:27 +0200

    refactor: adjust to bq connector class

--- bqml_registry/model_registry.py
+++ bqml_registry/model_registry.py
@@ -5,9 +5,10 @@ from .config import Config
 from .model_data import ModelData
 from .schemas import RegistrySchema
 from .model_names import ModelNames
+from .connector import BigQueryConnector
 
 
-class ModelRegistry(Config):
+class ModelRegistry():
     """Provides an interface for interacting with the model registry table."""
     
     def __init__(self, project_id: str, dataset_id: str, table_id: str) -> None:
@@ -15,6 +16,9 @@ class ModelRegistry(Config):
         self.dataset_id = dataset_id
         self.table_id = table_id
         self.full_table_id = f"{project_id}.{dataset_id}.{table_id}"
+
+        # Initialize BigQueryConnector object
+        self.connector = BigQueryConnector()
     
     def create_registry(self, schema: RegistrySchema) -> None:
         """Initialize or validate the registry table."""
@@ -24,7 +28,7 @@ class ModelRegistry(Config):
 
         else:
             table_definition = bigquery.Table(self.full_table_id, schema=schema.build_schema())
-            self.client.create_table(table_definition)
+            self.connector.client.create_table(table_definition)
             print(f"Table: {self.full_table_id} successfully created.")
 
     def add_model(self, model: ModelData) -> None:
@@ -33,16 +37,16 @@ class ModelRegistry(Config):
         # Fetching schema to automatially collect required metrics
         schema = self.fetch_schema()
 
-        # Create a dict with all model metadata
+        # Create a dict with general model metadata
         model_insert_dict = {
             "model_name": model.model_id,
             "created": model.created,
             "type": model.model_type,
             "target": model.fetch_target(),
-            "is_tunning": model.is_tunning,
+            "is_tunning": model.tuning,
         }
-
-        if any(field.name == 'features' for field in schema):
+        # if features.importance is in the schema, add feature importance to the dict
+        if any(field.name == 'features.importance' for field in schema):
             # Raise an error if model is not tree-based (no support for feature importance)
             if model.model_type not in ModelNames.TREE_MODELS:
                 raise ValueError("Feature importance can be only calculated for tree-based models.")
@@ -51,6 +55,7 @@ class ModelRegistry(Config):
         else:
             model_insert_dict["features"] = model.fetch_feature_names()
 
+        # Add additional model metadata to the dict
         metadata_dict = {
             "eval": model.fetch_eval_metrics(),
             "trainint_info": model.fetch_training_info(),
@@ -58,21 +63,23 @@ class ModelRegistry(Config):
         }
         model_insert_dict = model_insert_dict | metadata_dict
 
+        # Add tunning info if it exists
         if any(field.name == 'tunning' for field in schema):
             model_insert_dict["tunning"] = model.fetch_trial_info()
 
-        self.client.insert_rows_json(self.full_table_id, [model_insert_dict])
+        # Insert model metadata into the registry table
+        self.connector.client.insert_rows_json(self.full_table_id, [model_insert_dict])
 
     def fetch_schema(self):
         """Fetch model registry schema."""
-        table = self.client.get_table(self.full_table_id)
+        table = self.connector.client.get_table(self.full_table_id)
         return table.schema
     
     def _check_if_table_exists(self) -> bool:
         """Check if model registry exists."""
 
         try:
-            self.client.get_table(self.full_table_id)
+            self.connector.client.get_table(self.full_table_id)
             return True
         
         except NotFound:
