38ca4eeb5d4db27e96de3e599fcea4b3b33201ad
chrisb <chrism.budnik@gmail.com>
2023-10-01 14:17:40 +0200

    update: required permissions list

--- bqml_registry/permissions.py
+++ bqml_registry/permissions.py
@@ -3,8 +3,16 @@ from enum import Enum
 
 class RequiredPermissions(Enum):
     ALL = [
-        "bigquery.datasets.create",
-        "bigquery.datasets.get",
-        "bigquery.tables.create",
+        "bigquery.datasets.create"
+        "bigquery.datasets.get"
+        "bigquery.datasets.getIamPolicy"
+        "bigquery.models.getData"
+        "bigquery.models.getMetadata"
+        "bigquery.tables.create"
+        "bigquery.tables.get"
         "bigquery.tables.updateData"
+        "bigquery.jobs.create"
+        "bigquery.readsessions.create"
+        "bigquery.readsessions.getData"
+        "bigquery.jobs.listAll"
     ]
